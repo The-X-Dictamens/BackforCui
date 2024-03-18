f1e18c0b5a7d6e83e5acd65048868ae91d194cc3
GerardoGR7 <garza.robles.gerardo@gmail.com>
2024-03-17 20:12:48 -0600

    Mejoras en cartas y descripcion

--- landing.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('mouseover', () => {
-      card.querySelector('.card-body').style.display = 'block';
-    });
-  
-    card.addEventListener('mouseout', () => {
-      card.querySelector('.card-body').style.display = 'none';
-    });
-  });
